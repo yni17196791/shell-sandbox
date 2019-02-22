d6d033467378372d68476bd68b22204132022c0f
Yoshiki Niwa <yoshiki.niwa@gmail.com>
2019-02-22 16:22:54 +0900

    2019-02-22 [16:22]分の作業記録

--- sydlabs/a.sh
+++ sydlabs/a.sh
@@ -12,6 +12,6 @@ crumbs=$(sed -ne '/<div class="m-crumbs"/,/<\/div>/p' "$i" | sed -e 's/<[^>]*>//
 # 詳細を抜き出す
 # description=$(sed -ne '/<\!--bof Product description --/,/<\!--eof Product description -->/p' "$i" | sed -e 's/^[ ]*//g' | sed -e 's/<strong[^>]*>.*<\/strong[^>]*>//g' | tr -d '\n' | sed 's/<br[^>]*>/\n/Ig' | sed -e 's/<[^>]*>//g' | sed '/^$/d' | sed "1s/^/,<p>/" | sed "$ s/$/<p>/" | perl -pe 's/\n/<br>/g' | sed 's/\//＆#47;/g' | sed 's/\*/＆#42;/g' | sed 's/\+/＆#43;/g' | sed 's/\./＆#46;/g' | sed 's/\?/＆#63;/g' | sed 's/\[/＆#91;/g' | sed 's/\]/＆#93;/g' | sed 's/\^/＆#94;/g' | sed 's/\$/＆#36;/g' | sed 's/\-/＆#45;/g' )
 
-cat ${i%.*}.csv | sed "s/^/${crumbs}/g" >> output.csv
+cat ${i%.*}.csv | sed "s/^/${crumbs}/g" >> output.csv ;
 
 done
